c24940fe1937c7c2b00b422780c2a26baf7f766f
tanukili <108317269+tanukili@users.noreply.github.com>
2022-08-21 00:46:10 +0800

    layout04

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,3 +3,10 @@ console.log('Hello!');
 $(document).ready(() => {
   console.log('HesSchool Hello!');
 });
+
+$(document).ready(function(){
+  $('.hambuger').click(function (e) { 
+    e.preventDefault();
+    $('.top-menu ul').slideToggle(600);
+  });
+});
